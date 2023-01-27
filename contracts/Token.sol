79395ff31c673fa300542803265acf4c5ec206b6
adarshswaminath <adarshswaminath7@gmail.com>
2023-01-27 18:01:31 +0530

    Contract Tested

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -14,6 +14,12 @@ contract Token {
 
     function transfer(address to,uint256 amount) external {
         require(balances[msg.sender] >= amount,"Not enough money");
+        console.log(
+        "Transferring from %s to %s %s tokens",
+        msg.sender,
+        to,
+        amount
+    );
         balances[msg.sender] -= amount;
         balances[to] += amount;
         emit Transfer(msg.sender,to,amount);
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox")
 /** @type import('hardhat/config').HardhatUserConfig */
+import "hardhat/console.sol";
 module.exports = {
   solidity: "0.8.17",
 };
